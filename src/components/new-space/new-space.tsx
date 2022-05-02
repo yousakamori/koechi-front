af51002013ea058f1129b6b9a162b2c9e09bc9e3
yousakamori <yousakamori@gmail.com>
2022-05-02 21:33:00 +0900

    emoji pickerをdynamic importに変更

--- src/components/new-space/new-space.tsx
+++ src/components/new-space/new-space.tsx
@@ -1,5 +1,6 @@
 import { yupResolver } from '@hookform/resolvers/yup';
 import { NextSeo } from 'next-seo';
+import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
 import React, { useCallback, useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
@@ -9,13 +10,18 @@ import { Layout } from '@/components/common/layout';
 import { withLoginRequired } from '@/components/hoc/with-login-required';
 import { Button } from '@/components/ui/button';
 import { Container } from '@/components/ui/container';
-import { EmojiPicker } from '@/components/ui/emoji';
+import { EmojiPickerProps } from '@/components/ui/emoji';
 import { ErrorMessage } from '@/components/ui/error-message';
 import { Textarea } from '@/components/ui/textarea';
 import { Typography } from '@/components/ui/typography';
 import { createSpaceSchema } from '@/config/yup-schema';
 import { HttpError } from '@/error/http-error';
 import { SpaceDetails } from '@/types/space';
+// ___________________________________________________________________________
+//
+const EmojiPicker = dynamic<EmojiPickerProps>(() =>
+  import('@/components/ui/emoji').then((mod) => mod.EmojiPicker),
+);
 // ___________________________________________________________________________
 //
 type CreateValues = Pick<SpaceDetails, 'name' | 'emoji'>;
