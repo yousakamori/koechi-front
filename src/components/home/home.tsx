89105e3320e7cc5e279bc566c5b335166bd24f42
yousakamori <yousakamori@gmail.com>
2022-05-10 15:39:43 +0900

    topページのheroを削除

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -5,8 +5,6 @@ import { HomeTalksList } from './home-talks-list';
 import { Talk } from '@/types/talk';
 // ___________________________________________________________________________
 //
-const Hero = dynamic<object>(() => import('@/components/home/hero').then((mod) => mod.Hero));
-
 const HomeNotesList = dynamic<object>(() =>
   import('@/components/home/home-notes-list').then((mod) => mod.HomeNotesList),
 );
@@ -20,7 +18,6 @@ export type HomeProps = {
 export const Home: React.VFC<HomeProps> = ({ talks }) => {
   return (
     <Layout navbar>
-      <Hero />
       <HomeTalksList talks={talks} />
       <HomeNotesList />
     </Layout>
